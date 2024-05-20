fae451226f03862e55432c9ce3a2de2e0e7a4fe5
Jebb <jebb@timble.net>
2024-05-20 14:22:31 +0800

    Initial dev

--- src/App.vue
+++ src/App.vue
@@ -4,35 +4,10 @@ import BaseLayout from './BaseLayout.vue'
 import { RouterLink, RouterView } from 'vue-router'
 import Toast from 'primevue/toast'
 import { useToast } from 'primevue/usetoast'
-import Dialog from 'primevue/dialog';
-import Calendar from 'primevue/calendar'
-import { Api } from './api'
+
 
 const toast = useToast()
 const transactionModalVisible = ref(false)
-
-async function fetchBudgets() {
-  const api = new Api
-  budgets.value = await api.getBudgets()
-}
-
-async function fetchAccounts() {
-  const api = new Api
-  accounts.value = await api.getAccounts()
-}
-
-async function saveTransaction() {
-    const oDate = new Date(transactionDate.value);
-    const oMonth = oDate.getMonth() + 1
-    const budget_month = oMonth + '-' + oDate.getFullYear()
-
-    const api = new Api
-    await api.createTransaction(selectedBudget.value.budget_id, selectedAccount.value.account_id, amount.value, budget_month)
-
-    transactionModalVisible.value = false
-
-    // getSnapshots()
-}
 
 const tabs = ref([
     { label: 'Budget', icon: 'pi pi-envelope', route: '/' },
@@ -42,21 +17,6 @@ const tabs = ref([
     }}
 ])
 
-const payee = ref()
-const amount = ref()
-const budgets = ref()
-const accounts = ref()
-const selectedBudget = ref()
-const selectedAccount = ref()
-
-const transactionDate = ref(new Date())
-const today = new Date()
-today.setMonth(today.getMonth() + 2)
-
-onMounted(() => {
-  fetchBudgets()
-  fetchAccounts()
-});
 </script>
 
 <template>
@@ -87,35 +47,6 @@ onMounted(() => {
             </div>
         </template>
     </BaseLayout>
-
-    <template>
-        <div class="card flex flex-wrap gap-3 p-fluid">
-            <Dialog v-model:visible="transactionModalVisible" modal header="Add Transaction" :style="{ width: '25rem' }">
-                <div class="flex align-items-center gap-3 mb-5">
-                    <InputGroup>
-                        <InputGroupAddon>₱</InputGroupAddon>
-                        <InputNumber placeholder="Amount" v-model="amount" inputId="minmaxfraction" :minFractionDigits="2" :maxFractionDigits="5" />
-                    </InputGroup>
-                </div>
-                <div class="flex align-items-center gap-3 mb-5">
-                    <InputText placeholder="Payee" v-model="payee" class="w-full md:w-14rem" />
-                </div>
-                <div class="flex align-items-center gap-3 mb-5">
-                    <Dropdown v-model="selectedBudget" :options="budgets" filter optionLabel="title" placeholder="Budget" class="w-full md:w-14rem" @select="fetchBudgets"></Dropdown>
-                </div>
-                <div class="flex align-items-center gap-3 mb-5">
-                    <Dropdown v-model="selectedAccount" :options="accounts" filter optionLabel="title" placeholder="Account" class="w-full md:w-14rem" @select="fetchAccounts"></Dropdown>
-                </div>
-                <div class="flex align-items-center gap-3 mb-5">
-                    <Calendar v-model="transactionDate" showButtonBar showIcon class="w-full md:w-14rem" />
-                </div>
-                <div class="flex justify-content-end gap-2">
-                    <Button type="button" label="Cancel" severity="secondary" @click="transactionModalVisible = false"></Button>
-                    <Button type="button" label="Save" @click="saveTransaction"></Button>
-                </div>
-            </Dialog>
-        </div>
-    </template>
   
 </template>
 
--- src/views/BudgetView.vue
+++ src/views/BudgetView.vue
@@ -5,6 +5,7 @@ import { useToast } from 'primevue/usetoast'
 import DataTable from 'primevue/datatable'
 import Column from 'primevue/column'
 import Calendar from 'primevue/calendar'
+import Dialog from 'primevue/dialog';
 import Tag from 'primevue/tag'
 import { Api } from '../api'
 
@@ -21,7 +22,7 @@ const increaseCount = () => {
 }
 
 const getSnapshots = () => {
-  const oDate = new Date(date.value);
+  const oDate = new Date(date.value)
   const oMonth = oDate.getMonth() + 1
   
   const budget_month = oMonth + '-' + oDate.getFullYear()
@@ -31,11 +32,9 @@ const getSnapshots = () => {
   if (isProxy(budgets)) {
     const raw = toRaw(budgets)
 
-    // console.log(raw)
-
     raw.forEach(data => {
       if (data.month == budget_month) {
-        console.log(data.budgets)
+        // console.log(data.budgets)
         snapshots.value = data.budgets
       }
     })
@@ -52,15 +51,15 @@ const getSnapshots = () => {
 //   fetchSnapshots(budget_month)
 // }
 
-async function fetchSnapshots(budget_month: string) {
-  const api = new Api
+// async function fetchSnapshots(budget_month: string) {
+//   const api = new Api
 
-  if (budget_month) {
-    snapshots.value = await api.getSnapshotsBy(budget_month)
-  } else {
-    snapshots.value = await api.getSnapshots()
-  }
-}
+//   if (budget_month) {
+//     snapshots.value = await api.getSnapshotsBy(budget_month)
+//   } else {
+//     snapshots.value = await api.getSnapshots()
+//   }
+// }
 
 async function buildBudgetSnapshots() {
   const api = new Api
@@ -79,35 +78,50 @@ async function buildBudgetSnapshots() {
           assigned: data.assigned,
           available: data.available
       }
+
+      // console.log(endings)
   });
 
   // Build the budget snapshots data structure
   const data = []
+  const lastBudgetMonth = {}
+
   dates.forEach(month => {
       let oMonths = { month: month, budgets: [] = [] }
       
       budgets.forEach(budget => {
           let oBudgets = {}
           oBudgets.title = budget.title
-
-          let snaps = {}
           let hasSnapshot = false
 
           snapshots.forEach(snapshot => {
               if (budget.budget_id == snapshot.budget_id && month == snapshot.budget_month) {
                 oBudgets.assigned = snapshot.assigned
                 oBudgets.available = snapshot.available
+
                 hasSnapshot = true
-                  // snaps = {
-                  //     assigned: snapshot.assigned,
-                  //     available: snapshot.available
-                  // }
+
+                lastBudgetMonth[snapshot.title] = {
+                  title: snapshot.title,
+                  budget_month: snapshot.budget_month,
+                  assigned: snapshot.assigned,
+                  available: snapshot.available
+                }
               }
           })
-          
+
           if (!hasSnapshot) {
-              if (endings[budget.title]) {
-                  const oEnding = endings[budget.title]
+              // if (endings[budget.title]) {
+              //     const oEnding = endings[budget.title]
+
+              //     if (oEnding.budget_month != month) {
+              //       oEnding.assigned = 0
+              //     }
+
+              //     oBudgets = oEnding
+              if (lastBudgetMonth[budget.title]) {
+                console.log(lastBudgetMonth[budget.title])
+                const oEnding = lastBudgetMonth[budget.title]
 
                   if (oEnding.budget_month != month) {
                     oEnding.assigned = 0
@@ -127,7 +141,8 @@ async function buildBudgetSnapshots() {
   })
 
   budget_dates.value = data
-  // console.log(budget_dates.value)
+
+  getSnapshots()
 }
 
 const severity = (value) => {
@@ -138,16 +153,43 @@ const severity = (value) => {
   } else if (value < 0) {
     severity = 'danger'
   } else {
-    severity = 'warning'
+    severity = 'secondary'
   }
 
   return severity
 }
 
-onMounted(() => {
+const formatCurrency = (value: any) => {
+  return value.toLocaleString('en-US', { style: 'currency', currency: 'PHP' })
+}
+
+async function fetchBudgets() {
+  const api = new Api
+  budgets.value = await api.getBudgets()
+}
+
+async function fetchAccounts() {
+  const api = new Api
+  accounts.value = await api.getAccounts()
+}
+
+async function saveTransaction() {
+  const oDate = new Date(transactionDate.value);
+  const oMonth = oDate.getMonth() + 1
+  const budget_month = oMonth + '-' + oDate.getFullYear()
+
+  const api = new Api
+  await api.createTransaction(selectedBudget.value.budget_id, selectedAccount.value.account_id, amount.value, budget_month)
+
+  transactionModalVisible.value = false
   buildBudgetSnapshots()
-  getSnapshots()
-});
+}
+
+onMounted(() => {
+  fetchBudgets()
+  fetchAccounts()
+  buildBudgetSnapshots()
+})
 
 const snapshots = ref(null)
 const text = ref()
@@ -158,10 +200,14 @@ const today = new Date()
 today.setMonth(today.getMonth() + 2)
 const maxDate = ref(today)
 const budget_dates = ref(null)
-
-const formatCurrency = (value) => {
-  return value.toLocaleString('en-US', { style: 'currency', currency: 'PHP' })
-}
+const transactionModalVisible = ref(false)
+const selectedBudget = ref()
+const selectedAccount = ref()
+const payee = ref()
+const amount = ref()
+const budgets = ref()
+const accounts = ref()
+const transactionDate = ref(new Date())
 </script>
 
 <template>
@@ -170,6 +216,10 @@ const formatCurrency = (value) => {
   <template #header>
       <div class="card">
           <Toolbar style="padding: .5rem">
+            <template #start>
+              <Button icon="pi pi-plus" class="mr-2" severity="secondary" @click="transactionModalVisible = true" />
+            </template>
+
               <template #center>
                 <Calendar v-model="date" dateFormat="MM yy" showButtonBar view="month" :maxDate="maxDate" @date-select="getSnapshots" showIcon />
               </template>
@@ -179,7 +229,7 @@ const formatCurrency = (value) => {
 
   <template #default>
     <div class="card">
-      <DataTable stripedRows :value="snapshots" scrollable scrollHeight="300px" :virtualScrollerOptions="{ itemSize: 46 }" selectionMode="single">
+      <DataTable stripedRows :value="snapshots" scrollable scrollHeight="380px" :virtualScrollerOptions="{ itemSize: 46 }" selectionMode="single">
           <Column field="title"></Column>
           <Column field="assigned" header="Assigned" headerStyle="width: 7rem; text-align: right" bodyStyle="text-align: right">
             <template #body="slotProps">
@@ -203,6 +253,35 @@ const formatCurrency = (value) => {
         <label for="txt">Text label</label>
       </FloatLabel>
     </div> -->
+
+    <template>
+      <div class="card flex flex-wrap gap-3 p-fluid">
+        <Dialog v-model:visible="transactionModalVisible" modal header="Add Transaction" :style="{ width: '25rem' }">
+          <div class="flex align-items-center gap-3 mb-5">
+            <InputGroup>
+              <InputGroupAddon>₱</InputGroupAddon>
+              <InputNumber placeholder="Amount" v-model="amount" inputId="minmaxfraction" :minFractionDigits="2" :maxFractionDigits="5" />
+            </InputGroup>
+          </div>
+          <div class="flex align-items-center gap-3 mb-5">
+            <InputText placeholder="Payee" v-model="payee" class="w-full md:w-14rem" />
+          </div>
+          <div class="flex align-items-center gap-3 mb-5">
+            <Dropdown v-model="selectedBudget" :options="budgets" filter optionLabel="title" placeholder="Budget" class="w-full md:w-14rem" @select="fetchBudgets"></Dropdown>
+          </div>
+          <div class="flex align-items-center gap-3 mb-5">
+            <Dropdown v-model="selectedAccount" :options="accounts" filter optionLabel="title" placeholder="Account" class="w-full md:w-14rem" @select="fetchAccounts"></Dropdown>
+          </div>
+          <div class="flex align-items-center gap-3 mb-5">
+            <Calendar v-model="transactionDate" showButtonBar showIcon class="w-full md:w-14rem" />
+          </div>
+          <div class="flex justify-content-end gap-2">
+            <Button type="button" label="Cancel" severity="secondary" @click="transactionModalVisible = false"></Button>
+            <Button type="button" label="Save" @click="saveTransaction"></Button>
+          </div>
+        </Dialog>
+      </div>
+    </template>
   </template>
 
 </BaseLayout>
