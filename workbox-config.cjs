b293bcbe72efd77123eaabbf2e88f9dbbccb437f
Jebb <jebb@timble.net>
2024-08-02 19:18:14 +0800

    Fix workbox config

--- workbox-config.cjs
+++ workbox-config.cjs
@@ -1,13 +1,12 @@
 module.exports = {
 	globDirectory: 'dist/',
 	globPatterns: [
-		'**/*.{css,js,woff2,eot,svg,ttf,woff,ico,png,html,jpg,txt}',
-        '/index.html'
+		'**/*.{css,js,woff2,eot,svg,ttf,woff,ico,png,html,jpg,txt}'
 	],
-	swDest: 'dist/assets/sw.js',
+	swDest: 'dist/sw.js',
 	ignoreURLParametersMatching: [
 		/^utm_/,
 		/^fbclid$/
 	],
-    navigateFallback: '/index.html'
+    navigateFallback: null
 };
