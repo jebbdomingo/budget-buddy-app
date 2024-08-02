3b50235af2a00c30becdee386302e9d7b972afea
Jebb <jebb@timble.net>
2024-08-02 11:11:06 +0800

    Implement PWA via workbox-cli

---
+++ workbox-config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+	globDirectory: 'dist/',
+	globPatterns: [
+		'**/*.{css,js,woff2,eot,svg,ttf,woff,ico,png,html,jpg,txt}'
+	],
+	swDest: 'dist/sw.js',
+	ignoreURLParametersMatching: [
+		/^utm_/,
+		/^fbclid$/
+	]
+};
